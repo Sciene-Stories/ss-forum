cb7bfca2ed4c52834c3abfb5d0d70b2f6fb33b57
Bogdan Gewald <bogdygewald@yahoo.de>
2022-05-24 21:52:40 +0200

    install laravel breeze

---
+++ forum.md
@@ -0,0 +1,11 @@
+# Forum
+
+1. Thread
+2. Reply
+3. User
+4. Un Thread este realizat de un user
+5. Un reply belongs to thread, belongs to a user
+6. A Thread has a category, channel
+7. User are un profil
+8. Autentificare
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
     ],
@@ -13,23 +12,8 @@ module.exports = {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
-            colors: {
-                transparent: 'transparent',
-                current: 'currentColor',
-                'gray-background': '#f7f8fc',
-                'blue': '#328af1',
-                'blue-hover': '#2879bd',
-                'yellow' : '#ffc73c',
-                'red' : '#ec454f',
-                'green' : '#1aab8b',
-                'purple' : '#8b60ed',
-            },
         },
     },
 
-    plugins: [
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/line-clamp'),
-    ],
+    plugins: [require('@tailwindcss/forms')],
 };
